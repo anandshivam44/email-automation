1c1c5008864c4cf0d1e8b9c9a17767f397be1f14
anandshivam44 <anand.shivam44@yahoo.com>
2021-01-29 21:04:12 +0530

    Added Comments

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # Email Automation using SendGrid
 Send Emails using Node, Express and SendGrid
 ### Prerequisite
- - node and node package manager
- - Postman
-
-### How to setup in your localhost
- - Save your Api Key in your local environment
+- node and node package manager
+- Postman
+ 
+### How to set up in your localhost
+- Save your API Key in your local environment
 ```bash
 export API_KEY="Your custom API KEY from Sendgrid"
 ```
- - Install all dependencies listed in package.json
+- Install all dependencies listed in package.json
 ```bash
 npm install
 ```
- - To start the endpoint use
+- To start the endpoint use
 ```bash
 npm start
 ```
@@ -21,84 +21,84 @@ or
 ```bash
 node index.js
 ```
- - In the console check Port number
-   Example
-   ```bash
-   server started on PORT : 5000
-   ```
- - Open Postman and make a get request on http://localhost:5000 
-If You you get a callback as "Hello World", your server has started successfully on localhost  
-
+- In the console check Port number
+  Example
+  ```bash
+  server started on PORT: 5000
+  ```
+- Open Postman and make a get request on http://localhost:5000
+If You get a callback as "Hello World", your server has started successfully on localhost 
+ 
 ## How to Test the API
 ### Without attachment
-To send an email to a person copy paste this json Tree. This is the simplest payload. Every field is mandatory. Replece ``` emailMessage ``` with your HTML Body and ``` email ``` with an email id you want to send an email.
+To send an email to a person copy-paste this JSON Tree. This is the simplest payload. Every field is mandatory. Replace ``` emailMessage ``` with your HTML Body and ``` email ``` with an email id you want to send an email.
 ###### Sample Input
 ```js
 {
-    "userData": [
-        {
-            "email": "YOUR_EMAIL@PROVIDER.com",
-            "name": "Ram",
-            "status": "PASS",
-            "cc":[],
-            "bcc":[]
-        }
-    ],
-    "emailMessage": "<html><head><title></title></head><body>Hello {{name}} ,<br /><br/>Results are out<br /><br/><br /><br/>You are designated as {{status}}<br /><br/></body></html>"
+   "userData": [
+       {
+           "email": "YOUR_EMAIL@PROVIDER.com",
+           "name": "Ram",
+           "status": "PASS",
+           "cc":[],
+           "bcc":[]
+       }
+   ],
+   "emailMessage": "<html><head><title></title></head><body>Hello {{name}} ,<br /><br/>Results are out<br /><br/><br /><br/>You are designated as {{status}}<br /><br/></body></html>"
 }
 ```
-
+ 
 ###### Sample Output
 ```bash
 {
-    "status": 200,
-    "message": "Successfully added mails to queue",
-    "data": {
-        "success": [
-            "anand.shivam44@yahoo.com"
-        ],
-        "failure": [],
-        "note": "Check your Dashboard or set up a Webhook to get live values of failures. Emails are put in queue, they may take sometime to arrive or decide to fail"
-    }
+   "status": 200,
+   "message": "Successfully added mails to queue",
+   "data": {
+       "success": [
+           "anand.shivam44@yahoo.com"
+       ],
+       "failure": [],
+       "note": "Check your Dashboard or set up a Webhook to get live values of failures. Emails are put in a queue, they may take some time to arrive or decide to fail"
+   }
 }
 ```
 ###### Check Your MailBox </br>
-Check your mailbox to see the email you have sent. Emails are put in queue by sendgrid, they may take sometime to arrive or decide to fail.
-
+Check your mailbox to see the email you have sent. Emails are put in a queue by SendGrid, they may take some time to arrive or decide to fail.
+ 
 ##### More complex Input JSON samples
 ```bash
 {
-    "userData": [
-        {
-            "email": "EMAIL_1@PROVIDER.COM",
-            "name": "Ram",
-            "status": "PASS",
-            "cc":["EMAIL_2@PROVIDER.COM"],
-            "bcc":[]
-        },
-        {
-            "email": "EMAIL_1@PROVIDER.COM",
-            "name": "Ram",
-            "status": "PASS",
-            "cc":["EMAIL_3@PROVIDER.COM","EMAIL_4@PROVIDER.COM"],
-            "bcc":["EMAIL_5@PROVIDER.COM","EMAIL_6@PROVIDER.COM"]
-        }
-    ],
-    "emailMessage": "<html><head><title></title></head><body>Hello {{name}} ,<br /><br/>Results are out<br /><br/><br /><br/>You are designated as {{status}} This is the raw body<br /><br/></body></html>"
+   "userData": [
+       {
+           "email": "EMAIL_1@PROVIDER.COM",
+           "name": "Ram",
+           "status": "PASS",
+           "cc":["EMAIL_2@PROVIDER.COM"],
+           "bcc":[]
+       },
+       {
+           "email": "EMAIL_1@PROVIDER.COM",
+           "name": "Ram",
+           "status": "PASS",
+           "cc":["EMAIL_3@PROVIDER.COM","EMAIL_4@PROVIDER.COM"],
+           "bcc":["EMAIL_5@PROVIDER.COM","EMAIL_6@PROVIDER.COM"]
+       }
+   ],
+   "emailMessage": "<html><head><title></title></head><body>Hello {{name}} ,<br /><br/>Results are out<br /><br/><br /><br/>You are designated as {{status}} This is the raw body<br /><br/></body></html>"
 }
 ```
-### With attachment
-Attachments can se sent in an API using form-data and binary data. In binary data method one attachment can be sent with no body. Here form-data is used.
-
+### With attachment [PDF's]
+Attachments can se sent in an API using form-data and binary data. In binary data method one attachment can be sent with no json body. Here form-data is used.
+ 
 The easiest way to test the API with attachment
- - Open Postman
- - Select POST as a request
- - Add url http://localhost:5000
- - Click on Body Tab
- - Click ``` form-data ```
- - Add key as ```binaryFile``` In the same horizontal line click on dropdown select ```File``` and choose the file you want to send
- - Now fill all other options in the sample image
-
+- Open Postman
+- Select POST as a request
+- Add URL http://localhost:5000
+- Click on Body Tab
+- Click ``` form-data ```
+- Add key as ```binaryFile``` In the same horizontal line click on dropdown select ```File``` and choose the file you want to send
+- Now fill all other options in the sample image
+ 
 Key | Value |
 --- | --- |
 binaryFile | [CHOOSE FILE]] |
@@ -106,9 +106,9 @@ emailMessage | <p>Sample HTML Body</p> |
 userData[0][email] | EMAIL@PROVIDER.COM |
 userData[0][name] | Ram |
 userData[0][status] | PASS |
-
+ 
 ![with attachment image](image_2.png)
-
+ 
 ##### More complex sample imput
 Key | Value |
 --- | --- |
@@ -126,10 +126,10 @@ userData[1][cc] | EMAI5L@PROVIDER.COM |
 userData[1][cc] | EMAIL6@PROVIDER.COM |
 userData[1][bcc] | EMAIL7@PROVIDER.COM |
 userData[1][bcc] | EMAIL8@PROVIDER.COM |
-
-
+ 
+ 
 ## How to work with TypeScript and Node
-To compile your typescript file to javascript you must have type script installed. To install TypeScript
+To compile your typescript file to javascript you must have typescript installed. To install TypeScript
 ```bash
 sudo npm install -g typescript
 ```
--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
+/** Initializing Imports */
 var send: boolean = false;
 import Express = require("express");
-// var express = require('express');
 var app = Express();
 import http = require("https");
 var sgMail = require("@sendgrid/mail");
 import bodyParser = require('body-parser');
-
 import fs = require("fs");
-// const path = './test.pdf';
-var file_name = "";
 import multer = require("multer");
+
+/** Initializing multer to accept attachments */
+var file_name = "";
 const storage = multer.diskStorage({
    destination: function (req: any, file: any, cb: any) {
       file_name = file.originalname;
@@ -22,36 +22,48 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
+/** Add more configs */
 sgMail.setApiKey(process.env.API_KEY);
 sgMail.setSubstitutionWrappers('{{', '}}');
-
-
-
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+/** Simplest Route for testing */
 app.get('/', function (req, res) {
    res.send("Hello world!");
 });
 
+/** POST Route for sending e-mail(s) */
+/**
+ * @params
+ * start_time : Start time of request
+ * end_time : End time of request, used to find failed messages in a given time interval
+ * ll : no of customizations aka no of toS
+ * cc : cc of email body
+ * bcc : bcc of email body
+ * personalizations_block : Individual unit of userData
+ * set : stores all emails sent and prevents duplicate email sent if an email already exist
+ * fs
+ * unit : single customization block passed to SendGrid
+ * sgMail : SendGrid module to send emails easlily via SendGrid
+ */
 app.post('/test', upload.single('binaryFile'), function (req, res) {
-
+   /** Initializing vars */
    var start_time = Math.floor(new Date().getTime() / 1000);
    start_time -= 60;
 
    var user = req.body.userData;
+   
    var ll = Object.keys(user).length;
-   // console.log(req.body);
    var to = []
    var cc = [];
    var bcc = [];
    var sub = {}
    var personalizations_block: any = [];
-   console.log(ll);
-   console.log(user.toString + "\n\n");
-   var set = new Set();
-   for (let index = 0; index < ll; index++) {
 
+   var set = new Set();
+
+   for (let index = 0; index < ll; index++) {
       if (!set.has(user[index].email)) {
          set.add(user[index].email);
          to = [];
@@ -60,21 +72,41 @@ app.post('/test', upload.single('binaryFile'), function (req, res) {
          var details = {
             email: user[index].email
          }
-         to.push(details);
-         if (Object.keys(user[index].cc).length > 0) {
-            for (let i = 0; i < Object.keys(user[index].cc).length; i++) {
-               cc.push({"email":user[index].cc[i]});
-               set.add(user[index].cc[i]);
-               
+         to.push(details);// add 'to'
+
+         /** Add cc */
+         if (user[index].cc != null) {
+            console.log("There is at least 1 cc");
+            if (!Array.isArray(user[index].cc)) {
+               console.log("Lenght is 1");
+               cc.push({ "email": user[index].cc });
+               set.add(user[index].cc);
+            } else {
+               console.log("Lenght is > 1");
+               for (var i = 0; i < Object.keys(user[index].cc).length; i++) {
+                  cc.push({ "email": user[index].cc[i] });
+                  set.add(user[index].cc[i]);
+               }
             }
          }
-         if (Object.keys(user[index].bcc).length > 0) {
-            for (let i = 0; i < Object.keys(user[index].bcc).length; i++) {
-               bcc.push({"email":user[index].bcc[i]});
+
+         /** Add bcc */
+         if (user[index].bcc != null) {
+            console.log("There is at least 1 bcc");
+            if (!Array.isArray(user[index].bcc)) {
+               console.log("Lenght is 1");
+               bcc.push({ "email": user[index].bcc });
                set.add(user[index].bcc);
-               
+            } else {
+               console.log("Lenght is > 1");
+               for (var i = 0; i < Object.keys(user[index].bcc).length; i++) {
+                  bcc.push({ "email": user[index].bcc[i] });
+                  set.add(user[index].bcc[i]);
+               }
             }
          }
+
+         /** Combine all data to send to SendGrid */
          var unit = {
             'to': to,
             'cc': cc,
@@ -85,10 +117,11 @@ app.post('/test', upload.single('binaryFile'), function (req, res) {
             }
          };
          personalizations_block.push(unit);
-
       }
    }
-   fs.readFile((file_name), (err: any, data: any) => {
+
+   fs.readFile((file_name), (err: any, data: any) => {// read file
+      /** If there is no PDF attached execute this block */
       if (err) {
          console.log('PDF NOT FOUND');
          var msg = {
@@ -99,7 +132,7 @@ app.post('/test', upload.single('binaryFile'), function (req, res) {
          };
          console.log(msg.personalizations[0]);
          if (send) {
-            sgMail
+            sgMail//send mail
                .send(msg)
                .then(function () {
                   console.log('Email sent');
@@ -110,7 +143,8 @@ app.post('/test', upload.single('binaryFile'), function (req, res) {
                });
          }
       }
-      if (data) {//PDF FOUND
+      /** If there is a PDF attached execute this block */
+      if (data) {
          console.log('PDF FOUND');
          var msg1 = {
             from: 'shivamrocksdeoghar@gmail.com',
@@ -130,28 +164,31 @@ app.post('/test', upload.single('binaryFile'), function (req, res) {
          console.log(msg1.personalizations);
          if (send) {
             sgMail
-               .send(msg1)
-               .then(function () {
-                  console.log('Email sent');
-               })["catch"](function (error) {
-                  console.log("THERE IS An ERROR sending Email");
-                  console.error(error);
-                  res.status(403).send('Email could not be send! Check your input format');
+               .send(msg1)//send mail
+               .then(result => {
+                  console.log("Sent email");
+               }, err => {
+                  console.log("There is an error");
+                  console.error(err);
                });
          }
       }
    });
 
-   // try {
-   //    fs.unlinkSync(path)
-   //    console.log("FILE DELETE SUCCESSFUL")
-   //    //file removed
-   // } catch (err) {
-   //    console.error(err);
-   //    console.log("FILE COULD NOT BE DELETED");
-   // }
+   /** Delete Attachment after sending mail */
+   /**
+   *try {
+   *   fs.unlinkSync(path)
+   *   console.log("FILE DELETE SUCCESSFUL")
+   *   //file removed
+   *} catch (err) {
+   *   console.error(err);
+   *   console.log("FILE COULD NOT BE DELETED");
+   *}
+   */
 
 
+/** Call SendGrid to check for failed mails in a given time interval */
    var end_time = Math.floor(new Date().getTime() / 1000);
    var options = {
       "method": "GET",
@@ -181,6 +218,7 @@ app.post('/test', upload.single('binaryFile'), function (req, res) {
    failure_req.write("{}");
    failure_req.end();
 
+   /** Return Success */
    res.json({ 'status': 200, 'message': 'Successfully added mails to queue', 'data': { 'success': Array.from(set), 'failure': failure_body, 'note': 'Check your Dashboard or set up a Webhook to get live values of failures. Emails are put in queue, they may take sometime to arrive or decide to fail' } });
 });
 
