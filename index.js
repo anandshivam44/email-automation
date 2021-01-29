eeb930f2a92d823cc90c66ae1e1340bca4869b30
anandshivam44 <anand.shivam44@yahoo.com>
2021-01-29 21:06:18 +0530

    v1.1

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 "use strict";
 exports.__esModule = true;
+/** Initializing Imports */
 var send = false;
 var Express = require("express");
-// var express = require('express');
 var app = Express();
 var http = require("https");
 var sgMail = require("@sendgrid/mail");
 var bodyParser = require("body-parser");
 var fs = require("fs");
-// const path = './test.pdf';
-var file_name = "";
 var multer = require("multer");
+/** Initializing multer to accept attachments */
+var file_name = "";
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         file_name = file.originalname;
@@ -22,26 +22,40 @@ var storage = multer.diskStorage({
     }
 });
 var upload = multer({ storage: storage });
+/** Add more configs */
 sgMail.setApiKey(process.env.API_KEY);
 sgMail.setSubstitutionWrappers('{{', '}}');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+/** Simplest Route for testing */
 app.get('/', function (req, res) {
     res.send("Hello world!");
 });
+/** POST Route for sending e-mail(s) */
+/**
+ * @params
+ * start_time : Start time of request
+ * end_time : End time of request, used to find failed messages in a given time interval
+ * ll : no of customizations aka no of toS
+ * cc : cc of email body
+ * bcc : bcc of email body
+ * personalizations_block : Individual unit of userData
+ * set : stores all emails sent and prevents duplicate email sent if an email already exist
+ * fs
+ * unit : single customization block passed to SendGrid
+ * sgMail : SendGrid module to send emails easlily via SendGrid
+ */
 app.post('/test', upload.single('binaryFile'), function (req, res) {
+    /** Initializing vars */
     var start_time = Math.floor(new Date().getTime() / 1000);
     start_time -= 60;
     var user = req.body.userData;
     var ll = Object.keys(user).length;
-    // console.log(req.body);
     var to = [];
     var cc = [];
     var bcc = [];
     var sub = {};
     var personalizations_block = [];
-    console.log(ll);
-    console.log(user.toString + "\n\n");
     var set = new Set();
     for (var index = 0; index < ll; index++) {
         if (!set.has(user[index].email)) {
@@ -52,19 +66,40 @@ app.post('/test', upload.single('binaryFile'), function (req, res) {
             var details = {
                 email: user[index].email
             };
-            to.push(details);
-            if (Object.keys(user[index].cc).length > 0) {
-                for (var i = 0; i < Object.keys(user[index].cc).length; i++) {
-                    cc.push({ "email": user[index].cc[i] });
-                    set.add(user[index].cc[i]);
+            to.push(details); // add 'to'
+            /** Add cc */
+            if (user[index].cc != null) {
+                console.log("There is at least 1 cc");
+                if (!Array.isArray(user[index].cc)) {
+                    console.log("Lenght is 1");
+                    cc.push({ "email": user[index].cc });
+                    set.add(user[index].cc);
+                }
+                else {
+                    console.log("Lenght is > 1");
+                    for (var i = 0; i < Object.keys(user[index].cc).length; i++) {
+                        cc.push({ "email": user[index].cc[i] });
+                        set.add(user[index].cc[i]);
+                    }
                 }
             }
-            if (Object.keys(user[index].bcc).length > 0) {
-                for (var i = 0; i < Object.keys(user[index].bcc).length; i++) {
-                    bcc.push({ "email": user[index].bcc[i] });
+            /** Add bcc */
+            if (user[index].bcc != null) {
+                console.log("There is at least 1 bcc");
+                if (!Array.isArray(user[index].bcc)) {
+                    console.log("Lenght is 1");
+                    bcc.push({ "email": user[index].bcc });
                     set.add(user[index].bcc);
                 }
+                else {
+                    console.log("Lenght is > 1");
+                    for (var i = 0; i < Object.keys(user[index].bcc).length; i++) {
+                        bcc.push({ "email": user[index].bcc[i] });
+                        set.add(user[index].bcc[i]);
+                    }
+                }
             }
+            /** Combine all data to send to SendGrid */
             var unit = {
                 'to': to,
                 'cc': cc,
@@ -78,6 +113,7 @@ app.post('/test', upload.single('binaryFile'), function (req, res) {
         }
     }
     fs.readFile((file_name), function (err, data) {
+        /** If there is no PDF attached execute this block */
         if (err) {
             console.log('PDF NOT FOUND');
             var msg = {
@@ -88,7 +124,7 @@ app.post('/test', upload.single('binaryFile'), function (req, res) {
             };
             console.log(msg.personalizations[0]);
             if (send) {
-                sgMail
+                sgMail //send mail
                     .send(msg)
                     .then(function () {
                     console.log('Email sent');
@@ -99,7 +135,8 @@ app.post('/test', upload.single('binaryFile'), function (req, res) {
                 });
             }
         }
-        if (data) { //PDF FOUND
+        /** If there is a PDF attached execute this block */
+        if (data) {
             console.log('PDF FOUND');
             var msg1 = {
                 from: 'shivamrocksdeoghar@gmail.com',
@@ -119,25 +156,28 @@ app.post('/test', upload.single('binaryFile'), function (req, res) {
             console.log(msg1.personalizations);
             if (send) {
                 sgMail
-                    .send(msg1)
-                    .then(function () {
-                    console.log('Email sent');
-                })["catch"](function (error) {
-                    console.log("THERE IS An ERROR sending Email");
-                    console.error(error);
-                    res.status(403).send('Email could not be send! Check your input format');
+                    .send(msg1) //send mail
+                    .then(function (result) {
+                    console.log("Sent email");
+                }, function (err) {
+                    console.log("There is an error");
+                    console.error(err);
                 });
             }
         }
     });
-    // try {
-    //    fs.unlinkSync(path)
-    //    console.log("FILE DELETE SUCCESSFUL")
-    //    //file removed
-    // } catch (err) {
-    //    console.error(err);
-    //    console.log("FILE COULD NOT BE DELETED");
-    // }
+    /** Delete Attachment after sending mail */
+    /**
+    *try {
+    *   fs.unlinkSync(path)
+    *   console.log("FILE DELETE SUCCESSFUL")
+    *   //file removed
+    *} catch (err) {
+    *   console.error(err);
+    *   console.log("FILE COULD NOT BE DELETED");
+    *}
+    */
+    /** Call SendGrid to check for failed mails in a given time interval */
     var end_time = Math.floor(new Date().getTime() / 1000);
     var options = {
         "method": "GET",
@@ -163,6 +203,7 @@ app.post('/test', upload.single('binaryFile'), function (req, res) {
     });
     failure_req.write("{}");
     failure_req.end();
+    /** Return Success */
     res.json({ 'status': 200, 'message': 'Successfully added mails to queue', 'data': { 'success': Array.from(set), 'failure': failure_body, 'note': 'Check your Dashboard or set up a Webhook to get live values of failures. Emails are put in queue, they may take sometime to arrive or decide to fail' } });
 });
 app.listen(3000);
